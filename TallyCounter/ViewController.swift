8927e478147490c648168cc6e4c774522953b7eb
Gokul <gokul2594@outlook.com>
2019-05-21 18:34:57 -0400

    first commit

--- TallyCounter/ViewController.swift
+++ TallyCounter/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    var count: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.text = "0"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //dispose of any resources that can be recreated
     }
 
-
+    @IBAction func onIncrementClick(_ sender: Any) {
+        count += 1
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func onResetClick(_ sender: Any) {
+        count = 0
+        countLabel.text = "0"
+    }
 }
 
